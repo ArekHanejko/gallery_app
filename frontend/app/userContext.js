cd27fbcd78efad4e03a380bf3d8f8d43a6ab0c38
ArekHanejko <81444898+ArekHanejko@users.noreply.github.com>
2024-06-25 15:12:18 +0200

    Update userContext.js

--- frontend/app/userContext.js
+++ frontend/app/userContext.js
@@ -1,6 +1,6 @@
 "use client"
 import React, { createContext, useState, useEffect, useContext } from 'react';
-import Cookies from 'js-cookie';
+
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
@@ -8,15 +8,18 @@ export const UserProvider = ({ children }) => {
     const [userId, setUserId] = useState(null);
     const [userToken, setUserToken] = useState(null);
     const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         const storedUserName = localStorage.getItem('userName');
-        const storedUserToken = Cookies.get('userToken');
+        const storedUserToken = localStorage.getItem('userToken');
         const storedUserId = localStorage.getItem('userId');
+        
         if (storedUserName && storedUserId && storedUserToken) {
             setUserName(JSON.parse(storedUserName));
             setUserId(JSON.parse(storedUserId));
             setUserToken(storedUserToken);
         }
+        
         setLoading(false);
     }, []);
 
@@ -24,18 +27,20 @@ export const UserProvider = ({ children }) => {
         setUserName(userName);
         setUserId(userId);
         setUserToken(userToken);
+        
         localStorage.setItem('userName', JSON.stringify(userName));
         localStorage.setItem('userId', JSON.stringify(userId));
-        Cookies.set('userToken', userToken, { expires: 1 ,secure: false, sameSite: 'none'});
+        localStorage.setItem('userToken', userToken);
     };
 
     const logout = () => {
         setUserId(null);
         setUserName(null);
         setUserToken(null);
+        
         localStorage.removeItem('userName');
         localStorage.removeItem('userId');
-        Cookies.remove('userToken');
+        localStorage.removeItem('userToken');
     };
 
     return (
