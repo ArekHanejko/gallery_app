4fe24b4dc9c0be42ad17ec74d760dc03654aba0a
ArekHanejko <81444898+ArekHanejko@users.noreply.github.com>
2026-06-15 16:25:49 +0200

    Update database URL with new password

--- Backend/app/config.js
+++ Backend/app/config.js
@@ -1,6 +1,6 @@
 const config = {
     port: process.env.PORT || 3001,
-    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://arekimmobile_db_user:<db_password>@galleryapp.kjn0kkn.mongodb.net/?appName=galleryApp',
+    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://arekimmobile_db_user:FDLJxTdKh4qFUBO5@galleryapp.kjn0kkn.mongodb.net/?appName=galleryApp',
     JwtSecret: process.env.JWT_SECRET || 'secret'
 };
 export default config;
