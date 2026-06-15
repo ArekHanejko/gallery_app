aa10eb20c93524c637b6ea7b53eeaba543406d87
ArekHanejko <81444898+ArekHanejko@users.noreply.github.com>
2026-06-15 16:37:38 +0200

    t
    Added comment to clarify the purpose of the database URL.

--- Backend/app/config.js
+++ Backend/app/config.js
@@ -1,6 +1,6 @@
 const config = {
     port: process.env.PORT || 3001,
-    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://arekimmobile_db_user:FDLJxTdKh4qFUBO5@galleryapp.kjn0kkn.mongodb.net/?appName=galleryApp',
+    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://arekimmobile_db_user:FDLJxTdKh4qFUBO5@galleryapp.kjn0kkn.mongodb.net/?appName=galleryApp', //for test
     JwtSecret: process.env.JWT_SECRET || 'secret'
 };
 export default config;
